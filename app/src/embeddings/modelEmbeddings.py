6ecc011b42b7b404c65059d27f1daa34bf83c9c6
phaelzin <rapha555lima@gmail.com>
2025-01-06 17:49:09 -0300

    montando as estruturas iniciais do projeto

---
+++ app/src/embeddings/modelEmbeddings.py
@@ -0,0 +1,8 @@
+import os
+from typing import List, Any
+from langchain_openai import OpenAIEmbeddings
+
+class ModelEmbeddings:
+    def __init__(self, openai_api_key: str=None, model: str="text-embedding-3-small" ):
+        
+
